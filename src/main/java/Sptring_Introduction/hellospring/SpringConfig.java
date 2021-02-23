0e8e698551caac95c3e3604fe97067f2c55f8641
injunSeo <fregesaxiom@gmail.com>
2021-02-23 17:21:29 +0900

    회원관리 mvc 1. java Class: MemberController, MemberForm 2. HTML: createMemberForm, memberList

--- src/main/java/Sptring_Introduction/hellospring/SpringConfig.java
+++ src/main/java/Sptring_Introduction/hellospring/SpringConfig.java
@@ -8,10 +8,12 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfig {
+
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
     }
+
     @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
