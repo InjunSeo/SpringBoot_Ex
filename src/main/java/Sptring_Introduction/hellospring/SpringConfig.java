a5745f8b3e9eaddd832a196235b6fb36681b6578
injunSeo <fregesaxiom@gmail.com>
2021-02-24 17:49:25 +0900

    JdbcMemberRepository 작성 SpringConfig 수정 application.properties 작성

--- src/main/java/Sptring_Introduction/hellospring/SpringConfig.java
+++ src/main/java/Sptring_Introduction/hellospring/SpringConfig.java
@@ -1,13 +1,20 @@
 package Sptring_Introduction.hellospring;
 
+import Sptring_Introduction.hellospring.repository.JdbcMemberRepository;
 import Sptring_Introduction.hellospring.repository.MemberRepository;
-import Sptring_Introduction.hellospring.repository.MemoryMemberRepository;
 import Sptring_Introduction.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    private final DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,7 +23,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
+        /*return new MemoryMemberRepository();*/
     }
 
 }
